7baca502cb230a3b01e1bca40fff61234ca293af
rkpattanaik <rkpattanaik@live.com>
2020-01-14 10:33:14 +0100

    - Setup layout - Added GameFieldButton style

---
+++ app/src/main/java/com/cognizant/tictactoe/GameActivity.kt
@@ -0,0 +1,23 @@
+package com.cognizant.tictactoe
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fabRestart.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
